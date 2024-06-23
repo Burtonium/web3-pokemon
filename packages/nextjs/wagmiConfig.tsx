21e18ea6fa9c8127cafce45453c8db5a681f069e
Burtonium <9215927+Burtonium@users.noreply.github.com>
2024-06-24 02:45:38 +0700

    Add metamask settings

--- packages/nextjs/wagmiConfig.tsx
+++ packages/nextjs/wagmiConfig.tsx
@@ -4,7 +4,16 @@ import { injected, metaMask } from "wagmi/connectors";
 
 const config = createConfig({
   chains: [sepolia],
-  connectors: [injected(), metaMask()],
+  connectors: [
+    injected(),
+    metaMask({
+      dappMetadata: {
+        name: "Pokemon Web3",
+        url: process.env.NEXT_PUBLIC_VERCEL_URL,
+        base64Icon: "",
+      },
+    }),
+  ],
   transports: {
     [sepolia.id]: http("https://1rpc.io/sepolia"),
   },
